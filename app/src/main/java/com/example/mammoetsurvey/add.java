79370ceb169e904e2f488850c091b7da1a69b78f
lockmindF <lockser01@gmail.com>
2020-12-12 18:13:13 +0300

    Add screen

--- app/src/main/java/com/example/mammoetsurvey/add.java
+++ app/src/main/java/com/example/mammoetsurvey/add.java
@@ -2,13 +2,35 @@ package com.example.mammoetsurvey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class add extends AppCompatActivity {
+
+    ImageView chooseph;
+    Button choosebt;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        chooseph = findViewById(R.id.chooseIV);
+        choosebt = findViewById(R.id.choosePhoto);
+
+        choosebt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+                }
+            }
+        });
     }
 }
