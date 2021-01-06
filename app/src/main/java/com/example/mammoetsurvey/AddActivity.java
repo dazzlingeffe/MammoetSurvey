f750fe237b16ea4a55aa433bffc35fe30765d8d1
dazzlingeffe <irusya9@mail.ru>
2021-01-06 21:58:14 +0300

    current location

--- app/src/main/java/com/example/mammoetsurvey/AddActivity.java
+++ app/src/main/java/com/example/mammoetsurvey/AddActivity.java
@@ -3,11 +3,14 @@ package com.example.mammoetsurvey;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +18,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -22,6 +27,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
 
 import java.util.Map;
 
@@ -29,7 +37,9 @@ public class AddActivity extends AppCompatActivity implements OnMapReadyCallback
 
     ImageView chooseph;
     Button choosebt;
+    SupportMapFragment mapFragment;
     GoogleMap gMap;
+    FusedLocationProviderClient client;
     LatLng startPosition;
     LatLng endPosition;
     LatLng obstacle;
@@ -42,9 +52,15 @@ public class AddActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        client = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(AddActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions(AddActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION }, 44);
+        }
 
         chooseph = findViewById(R.id.chooseph);
         choosebt = findViewById(R.id.choosebt);
@@ -63,6 +79,36 @@ public class AddActivity extends AppCompatActivity implements OnMapReadyCallback
                     }
                 } else {
                     pickImageFromGallery();
+                }
+            }
+        });
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    mapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            obstacle = new LatLng(location.getLatitude(), location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(obstacle).title("Obstacle");
+                            gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(obstacle, 10));
+                            gMap.addMarker(options);
+                        }
+                    });
                 }
             }
         });
@@ -113,6 +159,11 @@ public class AddActivity extends AppCompatActivity implements OnMapReadyCallback
                     Toast.makeText(this, "Сперма коня!!!", Toast.LENGTH_SHORT).show();
                 }
             }
+            case 44: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getCurrentLocation();
+                }
+            }
         }
     }
 
@@ -124,4 +175,6 @@ public class AddActivity extends AppCompatActivity implements OnMapReadyCallback
             chooseph.setImageURI(data.getData());
         }
     }
+
+
 }
