f93c68b9f6e17083e4af5cd9bfe4b8797c698c3b
dazzlingeffe <irusya9@mail.ru>
2021-01-06 21:12:56 +0300

    dwudfhuew

--- app/src/main/java/com/example/mammoetsurvey/AddActivity.java
+++ app/src/main/java/com/example/mammoetsurvey/AddActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -14,10 +15,24 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class AddActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
+
+public class AddActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     ImageView chooseph;
     Button choosebt;
+    GoogleMap gMap;
+    LatLng startPosition;
+    LatLng endPosition;
+    LatLng obstacle;
 
     private static final int IMAGE_PICK_CODE = 1000;
     private static final int PERMISSION_CODE = 1001;
@@ -26,6 +41,10 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         chooseph = findViewById(R.id.chooseph);
         choosebt = findViewById(R.id.choosebt);
@@ -47,6 +66,35 @@ public class AddActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        // show current location
+
+
+//        set marker on click
+//        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//            @Override
+//            public void onMapClick(LatLng latLng) {
+//                MarkerOptions markerOptions = new MarkerOptions();
+//                markerOptions.position(latLng);
+//                markerOptions.title("Start");
+//                gMap.clear();
+//                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+//                gMap.addMarker(markerOptions);
+//                startPosition = latLng;
+//            }
+//        });
+
+
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        gMap.addMarker(new MarkerOptions()
+//                .position(sydney)
+//                .title("Marker in Sydney"));
+//        gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     private void pickImageFromGallery() {
@@ -71,6 +119,7 @@ public class AddActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
+        super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             chooseph.setImageURI(data.getData());
         }
