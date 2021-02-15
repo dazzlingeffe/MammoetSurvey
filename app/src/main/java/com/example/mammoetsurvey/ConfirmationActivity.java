3b4dec9af3ad932cbffd0c993cf43744befd2944
dazzlingeffe <irusya9@mail.ru>
2021-02-15 17:02:50 +0300

    displaying current location on ConfirmationActivity

--- app/src/main/java/com/example/mammoetsurvey/ConfirmationActivity.java
+++ app/src/main/java/com/example/mammoetsurvey/ConfirmationActivity.java
@@ -1,21 +1,35 @@
 package com.example.mammoetsurvey;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class ConfirmationActivity extends AppCompatActivity implements OnMapReadyCallback {
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
     Mark newMark;
     Route newRoute;
     GoogleMap gMap;
@@ -27,9 +41,12 @@ public class ConfirmationActivity extends AppCompatActivity implements OnMapRead
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapConfirm);
-        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLastLocation();
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.mapConfirm);
+//        mapFragment.getMapAsync(this);
 
 
         init();
@@ -48,6 +65,26 @@ public class ConfirmationActivity extends AppCompatActivity implements OnMapRead
         });
     }
 
+    private void fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.mapConfirm);
+                    mapFragment.getMapAsync(ConfirmationActivity.this::onMapReady);
+
+                }
+            }
+        });
+    }
+
     private void init() {
         newMark = Mark.getInstance();
         newRoute = Route.getInstance();
@@ -58,9 +95,22 @@ public class ConfirmationActivity extends AppCompatActivity implements OnMapRead
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
+        LatLng location = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
 
         gMap.addMarker(new MarkerOptions().position(newRoute.startPosition).title(newRoute.routeName + ": start"));
         gMap.addMarker(new MarkerOptions().position(newRoute.endPosition).title(newRoute.routeName + ": end"));
-        gMap.moveCamera(CameraUpdateFactory.newLatLng(newRoute.startPosition));
+        gMap.addMarker(new MarkerOptions().position(location).title("obstacle").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLastLocation();
+                }
+                break;
+        }
     }
 }
